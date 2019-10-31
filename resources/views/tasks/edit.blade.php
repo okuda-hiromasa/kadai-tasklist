12130d861a79ac3d7e2e99993e29f15c13fc23cf
okuda.hiromasa <ec2-user@ip-172-31-90-57.ec2.internal>
2019-10-31 11:27:36 +0000

    make Tasklist

---
+++ resources/views/tasks/edit.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!-- ここにページ毎のコンテンツを書く -->
+<h1>id: {{ $task->id}}のタスク編集ページ</h1>
+<div class="row">
+    <div class="col-6 ">
+    {!! Form::model($task,['route'=>['tasks.update',$task->id],'method'=>'put'])!!}    
+     
+<table class="table table-stiped">
+    <tr>
+        <th> {!! Form::label('title','タスク名:')!!}</th>
+        <td>{!! Form::text('title',null,['class'=>'btn btn-primay'])!!}</td>
+    <tr>
+        <th>{!! Form::label('content','内容:')!!}</th>
+        <td> {!! Form::text('content',null,['class'=>'btn btn-primay'])!!}</td>
+    </tr>
+    
+</table>
+ {!! Form::submit('更新',['class' => 'btn btn-primary'])!!}
+    {!! Form::close()!!}
+</div>
+@endsection
